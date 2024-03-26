aaa2eea164d0f88198eb243c604f1fc344a0d4d2
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-03-26 10:50:59 -0300

    init commit

---
+++ src/test/java/me/joaopdmcr/sdw/SantanderDevApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joaopdmcr.sdw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
